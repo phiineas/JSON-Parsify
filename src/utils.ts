69f1ebe65223665d10fe7da4e14d99cda7487636
pr0zpekt <you@example.com>
2024-07-20 22:42:15 +0530

    initial commit

--- src/utils.ts
+++
@@ -1,6 +0,0 @@
-export const isString = (value: string): boolean =>
-  value.startsWith('"') && value.endsWith('"');
-export const isBooleanTrue = (value: string): boolean => value === 'true';
-export const isBooleanFalse = (value: string): boolean => value === 'false';
-export const isNull = (value: string): boolean => value === 'null';
-export const isNumber = (value: string): boolean => !isNaN(Number(value));
