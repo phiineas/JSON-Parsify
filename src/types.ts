9f4d1cda22893c064a6bef0483a572b2f352f0f2
pr0zpekt <you@example.com>
2024-07-21 00:24:30 +0530

    have to do parser next

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type TokenType =
+  | "BraceOpen"
+  | "BraceClose"
+  | "BracketOpen"
+  | "BracketClose"
+  | "String"
+  | "Number"
+  | "Comma"
+  | "Colon"
+  | "True"
+  | "False"
+  | "Null";
+
+export interface Token {
+  type: TokenType;
+  value: string;
+}
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+export const isBooleanTrue = (value: string): boolean => value === "true";
+export const isBooleanFalse = (value: string): boolean => value === "false";
+export const isNull = (value: string): boolean => value === "null";
+export const isNumber = (value: string): boolean => !isNaN(Number(value));
